c8b147a6e3ad17540e4e4512d44e75f6cecd711e
amaya-dole <71594413+amaya-dole@users.noreply.github.com>
2022-11-04 23:01:17 +0530

    Update heartDisease.py
    hd

--- heartDisease.py
+++ heartDisease.py
@@ -1,5 +1,3 @@
-#!C:\Users\Lenovo\AppData\Local\Programs\Python\Python37-32\python.exe
-
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -9,29 +7,20 @@ import pickle
 warnings.filterwarnings("ignore")
 
 data = pd.read_csv("HeartDisease.csv")
-data = np.array(data)
-
-X = data[1:, 1:-1]
-y = data[1:, -1]
-y = y.astype('int')
-X = X.astype('int')
+x = data.drop('target',axis=1)
+y = data['target']
 
 # print(X,y)
 #split set traing set 30% testingset 70%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 rand_forest = RandomForestClassifier()
 
 
 #import pickel to train
 rand_forest.fit(X_train, y_train)
 
-#not in video
-#inputt=[int(x) for x in "45 32 60".split(' ')]
-#final=[np.array(inputt)]
 
 #dump model in write mode-wb
-pickle.dump(rand_forest,open('heartDiseaseModel.pkl','wb'))
+pickle.dump(rand_forest,open('heartPrediction.pkl','wb'))
 #readable mode-rb
-model=pickle.load(open('heartDiseaseModel.pkl','rb'))
-
-
+model=pickle.load(open('heartPrediction.pkl','rb'))
